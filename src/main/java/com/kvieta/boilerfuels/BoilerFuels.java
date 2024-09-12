10cf62edc15cdacbdb6a79d4673f7e1ab4bc6ad7
kvieta <sneak@lyci.de>
2024-09-12 23:25:12 +0100

    initial setup

--- src/main/java/com/kvieta/boilerfuels/BoilerFuels.java
+++ src/main/java/com/kvieta/boilerfuels/BoilerFuels.java
@@ -1,4 +1,4 @@
-package com.myname.mymodid;
+package com.kvieta.boilerfuels;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,13 +10,13 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 
-@Mod(modid = MyMod.MODID, version = Tags.VERSION, name = "MyMod", acceptedMinecraftVersions = "[1.7.10]")
-public class MyMod {
+@Mod(modid = BoilerFuels.MODID, version = Tags.VERSION, name = "BoilerFuels", acceptedMinecraftVersions = "[1.7.10]")
+public class BoilerFuels {
 
-    public static final String MODID = "mymodid";
+    public static final String MODID = "boilerfuels";
     public static final Logger LOG = LogManager.getLogger(MODID);
 
-    @SidedProxy(clientSide = "com.myname.mymodid.ClientProxy", serverSide = "com.myname.mymodid.CommonProxy")
+    @SidedProxy(clientSide = "com.kvieta.boilerfuels.ClientProxy", serverSide = "com.myname.mymodid.CommonProxy")
     public static CommonProxy proxy;
 
     @Mod.EventHandler
--- src/main/java/com/kvieta/boilerfuels/ClientProxy.java
+++ src/main/java/com/kvieta/boilerfuels/ClientProxy.java
@@ -1,4 +1,4 @@
-package com.myname.mymodid;
+package com.kvieta.boilerfuels;
 
 public class ClientProxy extends CommonProxy {
 
